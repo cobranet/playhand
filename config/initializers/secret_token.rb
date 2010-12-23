d5157e0192d905516c4845ba1e2e174517cfff45
cobranet <bratislav.petrovic@gmail.com>
2010-12-23 11:03:04 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playhand::Application.config.secret_token = 'f040444d3e3f56061dbbf7003d9918eb2fc6cbc5d1363b047b7e1d7380cda8a0baa867c15b4850921c493da6aacefdbc87635c7b11728065ab773b0723917e96'
