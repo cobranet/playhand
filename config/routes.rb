2730866d8095ac452a41baf767ee405b828eb832
cobranet <bratislav.petrovic@gmail.com>
2010-12-25 00:51:04 +0100

    users added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Playhand::Application.routes.draw do
+  devise_for :users
+
   resources :hands
 
   # The priority is based upon order of creation:
@@ -57,4 +59,5 @@ Playhand::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  root :to => "hands#index"
 end
